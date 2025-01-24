d04032d306d2691f45765137eb76178372e086fb
Omotara (Tara) Suleiman <107801016+babytara@users.noreply.github.com>
2025-01-24 15:10:44 -0500

    Update prototype1.py

--- prototype1.py
+++ prototype1.py
@@ -22,3 +22,38 @@ def edit_item(id, new_name=None, new_description=None, csv_filename='data.csv'):
     with open(csv_filename, mode='w', newline='') as csv_file:
             writer = csv.writer(csv_file)
             writer.writerows(updated_rows)
+
+# Menu option
+
+def main():
+    while True:
+        print("\nCatalog Management System")
+        print("1. View Catalog")
+        print("2. Add a new item")
+        print("3. Edit item description/name")
+        print("4. Delete item")
+        print("5. Exit program")
+        choice = input("Enter a choice: ")
+
+        if choice == "1":
+            view_catalog()
+        elif choice == "2":
+            add_item()
+        elif choice == "3":
+            edit_item()
+        elif choice == "4":
+            delete_item()
+        elif choice == "5":
+            print("Exiting program")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+
+# View catalog option - Display from array 
+    def view_catalog():
+        with open('catalog.csv', 'r') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                print(row)
+    
